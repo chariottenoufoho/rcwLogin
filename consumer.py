cb0a32ec7b34c73ee5679c84208ec867603cf413
chariottenoufoho <150136669+chariottenoufoho@users.noreply.github.com>
2024-07-05 08:20:17 -0400

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -1,1 +1,15 @@
+import requests
 
+url = 'https://rcw.azurewebsites.net/RCW_API'
+
+data = {
+    "first_name" : 'Steve',
+    'last_name' : 'Ataky'
+}
+
+response = requests.post(url, json=data)
+
+if response.status_code==200:
+    print(response.json())
+else:
+    print(f"Failed to connect to thw API. Status code: {response.status_code}")
